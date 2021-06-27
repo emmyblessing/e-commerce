52530740c59f6879a6d901bf795010697ba18bfb
emmyblessing <emmanuelolawole78@gmail.com>
2021-06-27 16:21:56 +0100

    e-commerce

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
-import { Link, /* useLocation */ } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import logo from '../../assets/commerce.png';
 import useStyles from './styles';
@@ -9,7 +9,7 @@ import useStyles from './styles';
 const PrimarySearchAppBar = ({ totalItems }) => {
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
   const classes = useStyles();
-  /* const location = useLocation(); */
+  const location = useLocation();
 
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
@@ -20,7 +20,7 @@ const PrimarySearchAppBar = ({ totalItems }) => {
   const renderMobileMenu = (
     <Menu anchorEl={mobileMoreAnchorEl} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} id={mobileMenuId} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right' }} open={isMobileMenuOpen} onClose={handleMobileMenuClose}>
       <MenuItem>
-        <IconButton /* component={Link} to="/cart" */ aria-label="Show cart items" color="inherit">
+        <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
           <Badge badgeContent={totalItems} color="secondary">
             <ShoppingCart />
           </Badge>
@@ -38,7 +38,7 @@ const PrimarySearchAppBar = ({ totalItems }) => {
             <img src={logo} alt="commerce.js" height="25px" className={classes.image} /> Commerce.js
           </Typography>
           <div className={classes.grow} />
-          {/* {location.pathname === '/' && ( */}
+          {location.pathname === '/' && (
           <div className={classes.button}>
             <IconButton  component={Link} to="/cart" aria-label="Show cart items" color="inherit">
               <Badge badgeContent={totalItems} color="secondary">
@@ -46,7 +46,7 @@ const PrimarySearchAppBar = ({ totalItems }) => {
               </Badge>
             </IconButton>
           </div>
-          {/* )} */}
+          )}
         </Toolbar>
       </AppBar>
       {renderMobileMenu}
